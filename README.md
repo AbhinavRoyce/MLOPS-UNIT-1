42912a90ae7b90c9a41f819bd24ca9ccc7c7315b
AbhinavRoyce <abhinava5p5@gmail.com>
2026-04-22 10:34:13 +0530

    Files

---
+++ README.md
@@ -0,0 +1,10 @@
+## Environment Setup
+
+To recreate this project's environment:
+
+1. Create a virtual environment: `python -m venv mlops`
+2. Activate it: `.\mlops\Scripts\activate`
+3. Install dependencies: `pip install -r requirements.txt`
+
+# For Mac/Linux:
+source mlops/bin/activate
--- src/train_model.py
+++ src/train_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_squared_error
 import joblib
 
 # Load dataset
@@ -17,7 +18,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Calculate MSE
+mse = mean_squared_error(y_test, y_pred)
+print("Model MSE:", mse)
+
 # Save model
 joblib.dump(model, '../models/iris_model.pkl')
 
-print("Model saved to models/iris_model.pkl")
+print("Model saved!")
